7fd71784cf72016c6a485b531092cd64fe92497b
Bradley Schill <88452819+Poiikm2@users.noreply.github.com>
2021-08-06 14:48:46 +0000

    WORK BITCH

---
+++ main.py
@@ -0,0 +1,77 @@
+import discord
+import os
+import asyncio
+import random
+
+client = discord.Client()
+
+bad_words = ["fuck", "Fuck", "Retard", "retard", "damn","sucks", "Sucks"]
+
+meepodefense = ["You should be nicer to <@159985870458322944> !", "Be Nice!", "You should be nicer!", "Not nice Akudama....", "You sound like an executer...", "AKUDAMA!", "I'm calling cut-throat...", "(╯°□°）╯︵ ┻━┻  .........  ┬─┬ ノ( ゜-゜ノ)", "Poof!", "<@159985870458322944> is doing his best ok!", "(╯°□°）╯︵ ┻━┻  AKUDAMA!"]
+
+mentionslist = ["<@!159985870458322944>", "<@!269705573795102720>"]
+
+emoji = '\N{OCTAGONAL SIGN}'
+#use emojipedia to find "unicode name"
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+    if any(word in msg for word in bad_words) and any(word in msg for word in mentionslist):
+        await message.channel.send(random.choice(meepodefense))
+        
+    elif "bitch" in message.content:
+        await message.channel.send('Only Akudama say bad words, but I do love dogs')
+
+        
+    elif "ass" in message.content:
+        await message.channel.send('Stop looking at mine!')
+
+        
+    elif "whore" in message.content:
+        await message.channel.send('Like your mom? #Swindled')
+
+
+    elif "damn" in message.content:
+        await message.channel.send('Like what the beavers live in?')
+
+        
+    elif "shit" in message.content:
+        await message.channel.send('You know, you can just go, No need to act like an Akudama')
+
+
+    elif "Bitch" in message.content:
+        await message.channel.send('Only Akudama say bad words, but I do love dogs')
+
+        
+    elif "Ass" in message.content:
+        await message.channel.send('Stop looking at mine!')
+
+        
+    elif "Whore" in message.content:
+        await message.channel.send('Like your mom? #Swindled')
+
+
+    elif "Damn" in message.content:
+        await message.channel.send('Like what the beavers live in?')
+
+        
+    elif "Shit" in message.content:
+        await message.channel.send('You know, you can just go, No need to act like an Akudama')
+
+
+    if any(word in msg for word in bad_words):
+        await message.add_reaction(emoji)
+
+
+client.run(os.getenv('TOKEN'))
+
